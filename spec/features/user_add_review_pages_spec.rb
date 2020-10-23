7d667fa2dc7c3879ee6ba866e163c50cc7dc53b9
Alma <a.q.frankenstein@gmail.com>
2020-10-23 14:11:07 -0700

    add spec for using adding a review

--- spec/features/user_add_review_pages_spec.rb
+++ spec/features/user_add_review_pages_spec.rb
@@ -3,16 +3,11 @@ require 'rails_helper'
 describe "add a review to a product" do
   it "adds a review to a product" do
     visit products_path
-    fill_in 'Email', :with => 'adminFriend@admin.com'
+    fill_in 'Email', :with => 'userfriend@user.com'
     fill_in 'Password', :with => 'asdfasdf'
     click_on 'Log in'
 
-    click_link 'Add new product'
-    fill_in 'Name', :with => 'Fennel'
-    fill_in 'Cost', :with => '2'
-    fill_in 'Country of origin', :with => 'Mexico'
-    click_on 'Create Product'
-    click_on 'Fennel'
+    click_on 'Bagel'
     click_on 'Add a review'
     fill_in 'Author', :with => 'Mike'
     fill_in 'Content body', :with => 'In season this month, especially good roasted with garlic and served with rice'
