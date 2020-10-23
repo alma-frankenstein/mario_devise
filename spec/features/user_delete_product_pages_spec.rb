78ad101f05b1580ebb4f542875f604e3250e0044
Alma <a.q.frankenstein@gmail.com>
2020-10-23 14:14:37 -0700

    add spec for using deleting product

--- spec/features/user_delete_product_pages_spec.rb
+++ spec/features/user_delete_product_pages_spec.rb
@@ -1,25 +1,17 @@
 require 'rails_helper'
 
-describe "deletes a product" do
-  it "adds a new product" do
+describe "if a non-admin tries to delete, they're directed back to the products page" do
+  it "doesn't let a non-admin delete" do
     visit products_path
-    fill_in 'Email', :with => 'adminFriend@admin.com'
+    fill_in 'Email', :with => 'userfriend@user.com'
     fill_in 'Password', :with => 'asdfasdf'
     click_on 'Log in'
 
-    click_link 'Add new product'
-    fill_in 'Name', :with => 'Fennel'
-    fill_in 'Cost', :with => '2'
-    fill_in 'Country of origin', :with => 'Mexico'
-    click_on 'Create Product'
-    click_on 'Fennel'
+    click_on 'Bagel'
     click_on 'Edit name of product'
     fill_in 'Name', :with => 'Bagel'
-    click_on 'Update Product'
-    expect(page).to have_content 'Bagel'
-    click_on 'Bagel'
     click_on 'Delete this product'
-    expect(page).not_to have_content 'Bagel'
+    expect(page).to have_content 'Bagel'
 
     click_link 'Logout'
   end
