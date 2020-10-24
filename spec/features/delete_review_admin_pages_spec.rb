92ea8b71931aeecd4c942b4cc97411e159219f8f
Alma <a.q.frankenstein@gmail.com>
2020-10-23 17:05:30 -0700

    add schmaltz to make test more concise

--- spec/features/delete_review_admin_pages_spec.rb
+++ spec/features/delete_review_admin_pages_spec.rb
@@ -7,12 +7,7 @@ describe "delete a review" do
     fill_in 'Password', :with => 'asdfasdf'
     click_on 'Log in'
 
-    click_link 'Add new product'
-    fill_in 'Name', :with => 'Fennel'
-    fill_in 'Cost', :with => '2'
-    fill_in 'Country of origin', :with => 'Mexico'
-    click_on 'Create Product'
-    click_on 'Fennel'
+    click_on 'Schmaltz'
     click_on 'Add a review'
     fill_in 'Author', :with => 'Mike'
     fill_in 'Content body', :with => 'In season this month, especially good roasted with garlic and served with rice'
